a68896bc478438a38628f2a8a0e29f106779f68a
alejocram <alejocram@gmail.com>
2016-05-18 01:52:18 -0500

    Se crea las funciones en el modelo para consular el CoreData y Guardar el registro de Restaurante

--- Restaurants/Models/RestaurantModel.swift
+++ Restaurants/Models/RestaurantModel.swift
@@ -7,11 +7,90 @@
 //
 
 import UIKit
+import CoreData
 
 typealias CompletionHandler = (success: Bool, response: [Restaurant]) ->()
 
 class RestaurantModel: NSObject {
     var restaurants = [Restaurant]()
+    
+    var managedObjects = [NSManagedObject]()
+    
+    //MARK: CoreData
+    
+    func saveRestaurant(restaurant: Restaurant) throws {
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        let entity = NSEntityDescription.entityForName("Restaurants", inManagedObjectContext: managedContext)
+        let managedObject = NSManagedObject(entity: entity!, insertIntoManagedObjectContext:managedContext)
+        
+        managedObject.setValue(restaurant.name, forKey: "name")
+        managedObject.setValue(restaurant.address , forKey: "address")
+        managedObject.setValue(restaurant.details, forKey: "details")
+        managedObject.setValue(restaurant.lat, forKey: "lat")
+        managedObject.setValue(restaurant.lon, forKey: "lon")
+        managedObject.setValue(restaurant.category, forKey: "category")
+        managedObject.setValue(restaurant.schedule, forKey: "schedule")
+        managedObject.setValue(restaurant.paymentMethods, forKey: "paymentMethods")
+        managedObject.setValue(restaurant.wifi, forKey: "wifi")
+        managedObject.setValue(restaurant.playground, forKey: "playground")
+        managedObject.setValue(restaurant.webPage, forKey: "webPage")
+        managedObject.setValue(restaurant.ranking, forKey: "ranking")
+        managedObject.setValue(restaurant.image, forKey: "image")
+        
+        do {
+            try managedContext.save()
+            managedObjects.append(managedObject)
+            manageObjectToRestauran()
+//            return true
+//            self.makeAlert("El restaurante ha sido guardado", type: .Default)
+        } catch let error as NSError{
+            print("No se pude guardar los datos, Error: \(error.localizedDescription)")
+            throw error
+//            self.makeAlert("Ha ocurrido un error al guardar el restaurante: \(error.description)", type: .Default)
+        }
+    }
+    
+    func fetchRestaurants() throws {
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let managedContext = appDelegate.managedObjectContext
+        
+        let fetch = NSFetchRequest(entityName: "Restaurants")
+        
+        do {
+            let result = try managedContext.executeFetchRequest(fetch)
+            managedObjects = result as! [NSManagedObject]
+            return manageObjectToRestauran()
+        } catch let error as NSError {
+            print("No pude recuperar datos, Error: \(error.localizedDescription)")
+            throw error
+        }
+    }
+    
+    func manageObjectToRestauran(){
+        
+        restaurants.removeAll()
+        for managedObject in managedObjects {
+            let restaurantTmp = Restaurant(
+                name: managedObject.valueForKey("name") as! String,
+                address: managedObject.valueForKey("address") as! String,
+                details: managedObject.valueForKey("details") as! String,
+                lat: managedObject.valueForKey("lat") as! Double,
+                lon: managedObject.valueForKey("lon") as! Double,
+                category: managedObject.valueForKey("category") as! String,
+                schedule: managedObject.valueForKey("schedule") as! String,
+                paymentMethods: managedObject.valueForKey("paymentMethods") as! String,
+                wifi: managedObject.valueForKey("wifi") as! Bool,
+                playground: managedObject.valueForKey("playground") as! Bool,
+                webPage: managedObject.valueForKey("webPage") as! String,
+                ranking: managedObject.valueForKey("ranking") as! Float,
+                image: managedObject.valueForKey("image") as! String)
+            
+            restaurants.append(restaurantTmp)
+        }
+    }
     
     //MARK: Services
     
@@ -25,9 +104,18 @@ class RestaurantModel: NSObject {
                     let restaurantTmp = Restaurant(name: item["name"] as! String, address: item["address"] as! String, details: item["details"] as! String, lat: item["lat"] as! Double, lon: item["lon"] as! Double, category: item["category"] as! String, schedule: item["schedule"] as! String, paymentMethods: item["paymentMethods"] as! String, wifi: item["wifi"] as! Bool, playground: item["playground"] as! Bool, webPage: item["webPage"] as! String, ranking: item["ranking"] as! Float, image: item["image"] as! String)
                     
                     self.restaurants.append(restaurantTmp)
-                    
-                    completion(success: true, response: self.restaurants)
                 }
+                
+                for restaurantTmp in self.restaurants {
+                    do{
+                        try self.saveRestaurant(restaurantTmp)
+                    } catch let error as NSError {
+                        print(error.localizedDescription)
+                        completion(success: false, response: self.restaurants)
+                    }
+                }
+                
+                completion(success: true, response: self.restaurants)
             } else {
                 completion(success: false, response: self.restaurants)
             }
